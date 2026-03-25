0a93970c06eb163a0c2e836eecb01f6f225a3759
rifqidwm <rifqidwimuslim2510@gmail.com>
2026-03-25 08:56:35 +0700

    Create User.java

---
+++ SleepTracker/User.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class User {
+    private String nama;
+    private ArrayList<SesiTidur> daftarSesi;
+
+    public User(String nama) {
+        this.nama = nama;
+        this.daftarSesi = new ArrayList<>();
+    }
+
+    public void tambahSesi(SesiTidur sesi) {
+        daftarSesi.add(sesi);
+    }
+
+    public void tampilkanLaporan() {
+        System.out.println("Laporan Tidur: " + nama);
+        for (SesiTidur s : daftarSesi) {
+            System.out.println(s.getRingkasan());
+        }
+    }
+    public ArrayList<SesiTidur> getDaftarSesi() {
+    return daftarSesi;
+}
+}
